953001e76180c6201079a2b0264eefe763263845
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-02-10 18:02:49 +0900

    テスト用コンポーネントの追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,15 @@
 import { Header } from './Header';
 import { Box, Button, Paper, Typography } from '@mui/material';
 import { TokensProvider } from '../contexts/tokensContext';
+import { sbot_test } from '../lib/switchbot';
+import { TestButton } from './TestButton';
 
 export const Home = () => {
 	return (
 		<>
 			<TokensProvider>
 				<Header />
-				<Button variant="contained" onClick={async () => {}}>
-					Contained
-				</Button>
+				<TestButton />
 			</TokensProvider>
 		</>
 	);
---
+++ src/lib/switchbot.ts
@@ -0,0 +1,5 @@
+import { Tokens } from '../types';
+
+export const sbot_test = (token: Tokens): string => {
+	return 'test';
+};
