aec861d88d2454617115a95f1425478154b7961f
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-02-24 08:16:52 +0900

    トップレベルawaitの許可

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		topLevelAwait({
+			// The export name of top-level await promise for each chunk module
+			promiseExportName: '__tla',
+			// The function to generate import names of top-level await promise in each chunk module
+			promiseImportName: (i) => `__tla_${i}`,
+		}),
+	],
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	// prevent vite from obscuring rust errors
@@ -18,8 +27,7 @@ export default defineConfig({
 	envPrefix: ['VITE_', 'TAURI_'],
 	build: {
 		// Tauri supports es2021
-		target:
-			process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+		target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
 		// don't minify for debug builds
 		minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
 		// produce sourcemaps for debug builds
